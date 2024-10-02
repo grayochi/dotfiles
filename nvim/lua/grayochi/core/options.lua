9e3ede3d583d4b6fe13f4b4d3226f8a822c6918a
grayochi <puelingki@gmail.com>
2024-10-02 14:39:20 +0800

    Update nvim options

--- nvim/lua/grayochi/core/options.lua
+++ nvim/lua/grayochi/core/options.lua
@@ -2,9 +2,41 @@ vim.cmd("let g:netrw_liststyle = 3")
 
 local o = vim.opt
 
-o.number = true
+-- encoding
+o.encoding = 'utf-8'
+o.fileencoding = 'utf-8'
 
-o.relativenumber = true
+o.number = true
+o.mouse = 'a'
+o.wrap = false
+o.clipboard = [[unnamed,unnamedplus]]
+o.hidden = true
+o.visualbell = true
+o.errorbells = false
+o.backspace = [[indent,eol,start]]
+
+-- turn off swap files.
+o.swapfile = false
+o.backup = false
+o.wb = false
+
+-- search settings
+o.incsearch = true
+o.hlsearch = true
+o.ignorecase = true
+o.smartcase = true
+
+-- auto compeletion
+o.wildmenu = true
+o.wildignore = { '*.o', '*.obj', '*.~'}
+o.wildignore.append = { '*DS_Store*'}
+o.wildignore.append = { '*.gem'}
+o.wildignore.append = { 'log/**'}
+o.wildignore.append = { 'tmp/**'}
+o.wildignore.append = { '*.png', '*.jpg', '*.gif'}
+
+-- turn off model
+o.modeline = false
 
 -- tab && indentation
 o.autoindent = true
@@ -13,14 +45,8 @@ o.shiftwidth = 0
 o.softtabstop = -1 -- 0 for tabstop, -1 for shiftwidth
 o.expandtab = true
 
--- search settings
-o.incsearch = true
-o.hlsearch = true
-o.ignorecase = true
-o.smartcase = true
+o.showmatch = true
 
--- backspace
-o.backspace = [[indent,eol,start]]
+o.list = true
+o.listchars=[[tab:\ \,trail:·]]
 
--- clipboard
-o.clipboard = [[unnamed,unnamedplus]]
